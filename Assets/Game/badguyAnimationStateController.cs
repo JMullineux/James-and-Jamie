10314a31e2a7ccda52a13dd099a8434995844794
James Mullineux <theJamesMullineux@gmail.com>
2020-11-27 17:43:18 +0000

    Ragdoll of the enemy can now be toggled (still need to add logic to do this on death; that is when they are hit by a bullet) I will also add code to launch the enemy in the appropriate direction on death; depending on where the bullet hits the enemy

--- Assets/Game/badguyAnimationStateController.cs
+++ Assets/Game/badguyAnimationStateController.cs
@@ -6,11 +6,18 @@ public class badguyAnimationStateController : MonoBehaviour
 {
 
     public Animator animator;
+    public Rigidbody RIGID_BODY;
+    public List<Collider> RagdollParts = new List<Collider>();
 
     // Start is called before the first frame update
     void Start()
     {
+        SetRagdollParts();
+        SetKinematic();
+        Physics.IgnoreLayerCollision(9, 10, true); //Box Collider and the Ragdoll do no collide with eachother
         Invoke("EquipWeapon", 4);
+        Invoke("TurnOnRagdoll", 8);
+        
     }
 
     // Update is called once per frame
@@ -23,5 +30,65 @@ public class badguyAnimationStateController : MonoBehaviour
     {
         animator.SetBool("isEquipping", true);
     }
+
+    void SetRagdollParts()
+    {
+        Collider[] colliders = this.gameObject.GetComponentsInChildren<Collider>();
+
+        foreach(Collider c in colliders)
+        {
+            if (c.gameObject != this.gameObject)
+            {
+                c.isTrigger = true;
+                RagdollParts.Add(c);
+            }
+
+            
+        }
+    }
+
+    void SetKinematic()
+    {
+        Rigidbody[] bodies = this.gameObject.GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in bodies)
+        {
+            if (rb.gameObject != this.gameObject)
+            {
+                rb.isKinematic = true;
+            }
+            
+            //RIGID_BODY.isKinematic = false;
+        }
+    }
+
+    void TurnOnRagdoll()
+    {
+        RIGID_BODY.useGravity = false;
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        animator.enabled = false;
+
+        foreach(Collider c in RagdollParts)
+        {
+            c.isTrigger = false;
+        }
+
+        Rigidbody[] bodies = this.gameObject.GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in bodies)
+        {
+            if (rb.gameObject != this.gameObject)
+            {
+                rb.isKinematic = false;
+            }
+
+            //RIGID_BODY.isKinematic = false;
+        }
+    }
+
+    void Die()
+    {
+
+    }
+
+       
 
 }
